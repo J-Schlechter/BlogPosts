507ebde376aa21b5484297c5bad2f74fd39a9126
J-Schlechter <j.schlechter.97@gmail.com>
2023-07-25 02:45:17 +0200

    Basic vue  navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,15 +7,14 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
   </head>
   <body>
-    <div id="navigation">
+    <div id="app">
       <navigation />
+      <hello /> 
     </div>
     <br>
-    <div id="hello">
-        <hello />       
+    
     </div>
-    </div>
-    <script src="{{mix('js/app.js')}}"></script>
+    <script defer src="{{mix('js/app.js')}}"></script>
   </body>
   
 </html>
